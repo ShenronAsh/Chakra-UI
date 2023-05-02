ac64e36d401f7d4502461f6c2c855de05dda5e00
ShenronAsh <ashishfan2@gmail.com>
2023-05-02 23:08:53 +0530

    first commit

---
+++ src/ColorModeSwitcher.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useColorMode, useColorModeValue, IconButton } from '@chakra-ui/react';
+import { FaMoon, FaSun } from 'react-icons/fa';
+
+  const ColorModeSwitcher = props => {
+  const { toggleColorMode } = useColorMode();
+  const SwitchIcon = useColorModeValue(FaMoon, FaSun);
+
+  return (
+    <IconButton
+    zIndex={'overlay'}
+      variant="ghost"
+      color="current"
+      pos={'fixed'}
+      top={'4px'}
+      right={'4px'}
+      marginLeft="2"
+      onClick={toggleColorMode}
+      icon={<SwitchIcon />}
+      {...props}
+    />
+  );
+};
+
+export default ColorModeSwitcher;
